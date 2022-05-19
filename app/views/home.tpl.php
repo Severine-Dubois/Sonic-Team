9ad60c4ba87abe8738b5c0907e757085d4e5b291
Severine-Dubois <sevdbs@gmail.com>
2022-05-19 11:16:50 +0200

    css no responsive yet

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,8 +1,11 @@
 <h2>SONIC ET SES COPAINS</h2>
 
+<div class = "articles">
+<section class="container">
 
 <?php foreach ($characters as $character) : ?>
 <article>
+    <div class="left">
         <h3>
         <?= $character->getName(); ?> - <?php foreach($types as $type):?>
         <?= $type->getId()==$character->getTypeId()? $type->getName().'</h3>' :''?>
@@ -10,7 +13,9 @@
         <p><?= $character->getDescription(); ?></p>
     </div>
 
+    <div class="right">
         <img src="<?= $absoluteURL ?>/assets/images/<?= $character->getPicture() ?>" alt="<?= $character->getName(); ?>">
+    </div>
     
 </article>
 
